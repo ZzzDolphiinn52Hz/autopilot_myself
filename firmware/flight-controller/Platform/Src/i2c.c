6ee088e32a9aecef8eb657b166e10b12a345f680
NTKkhanhnek <ntkkhanhnek06@gmail.com>
2026-07-29 18:53:28 +0700

    fix(i2c): Delete i2c_mem_read, i2c_mem_write, use basic transmit, receive function to replace

--- firmware/flight-controller/Platform/Src/i2c.c
+++ firmware/flight-controller/Platform/Src/i2c.c
@@ -1,15 +1,8 @@
 #include "i2c.h"
 #include "clock.h"
 
-#define GPIOA_ADDR      0x40020000
 #define GPIOB_ADDR      0x40020400
-#define I2C3_ADDR       0x40005C00
-
-static volatile uint32_t *GPIOA_MODER   = (volatile uint32_t *)(GPIOA_ADDR + 0x00);
-static volatile uint32_t *GPIOA_OTYPER  = (volatile uint32_t *)(GPIOA_ADDR + 0x04);
-static volatile uint32_t *GPIOA_OSPEEDR = (volatile uint32_t *)(GPIOA_ADDR + 0x08);
-static volatile uint32_t *GPIOA_PUPDR   = (volatile uint32_t *)(GPIOA_ADDR + 0x0C);
-static volatile uint32_t *GPIOA_AFRH    = (volatile uint32_t *)(GPIOA_ADDR + 0x24);
+#define I2C1_ADDR       0x40005400
 
 static volatile uint32_t *GPIOB_MODER   = (volatile uint32_t *)(GPIOB_ADDR + 0x00);
 static volatile uint32_t *GPIOB_OTYPER  = (volatile uint32_t *)(GPIOB_ADDR + 0x04);
@@ -17,14 +10,14 @@ static volatile uint32_t *GPIOB_OSPEEDR = (volatile uint32_t *)(GPIOB_ADDR + 0x0
 static volatile uint32_t *GPIOB_PUPDR   = (volatile uint32_t *)(GPIOB_ADDR + 0x0C);
 static volatile uint32_t *GPIOB_AFRL    = (volatile uint32_t *)(GPIOB_ADDR + 0x20);
 
-static volatile uint32_t *I2C3_CR1      = (volatile uint32_t *)(I2C3_ADDR + 0x00);
-static volatile uint32_t *I2C3_CR2      = (volatile uint32_t *)(I2C3_ADDR + 0x04);
-static volatile uint32_t *I2C3_OAR1     = (volatile uint32_t *)(I2C3_ADDR + 0x08);
-static volatile uint32_t *I2C3_DR       = (volatile uint32_t *)(I2C3_ADDR + 0x10);
-static volatile uint32_t *I2C3_SR1      = (volatile uint32_t *)(I2C3_ADDR + 0x14);
-static volatile uint32_t *I2C3_SR2      = (volatile uint32_t *)(I2C3_ADDR + 0x18);
-static volatile uint32_t *I2C3_CCR      = (volatile uint32_t *)(I2C3_ADDR + 0x1C);
-static volatile uint32_t *I2C3_TRISE    = (volatile uint32_t *)(I2C3_ADDR + 0x20);
+static volatile uint32_t *I2C1_CR1      = (volatile uint32_t *)(I2C1_ADDR + 0x00);
+static volatile uint32_t *I2C1_CR2      = (volatile uint32_t *)(I2C1_ADDR + 0x04);
+static volatile uint32_t *I2C1_OAR1     = (volatile uint32_t *)(I2C1_ADDR + 0x08);
+static volatile uint32_t *I2C1_DR       = (volatile uint32_t *)(I2C1_ADDR + 0x10);
+static volatile uint32_t *I2C1_SR1      = (volatile uint32_t *)(I2C1_ADDR + 0x14);
+static volatile uint32_t *I2C1_SR2      = (volatile uint32_t *)(I2C1_ADDR + 0x18);
+static volatile uint32_t *I2C1_CCR      = (volatile uint32_t *)(I2C1_ADDR + 0x1C);
+static volatile uint32_t *I2C1_TRISE    = (volatile uint32_t *)(I2C1_ADDR + 0x20);
 
 #define I2C_CR1_PE      (1 << 0)
 #define I2C_CR1_START   (1 << 8)
@@ -57,13 +50,13 @@ I2C_Status_t i2c_master_receive(uint8_t dev_addr, uint8_t *data, uint16_t len);
 
 I2C_Status_t i2c_start(void)
 {
-    *I2C3_CR1 |= I2C_CR1_START;
-    return i2c_wait_flag_set(I2C3_SR1, I2C_SR1_SB);
+    *I2C1_CR1 |= I2C_CR1_START;
+    return i2c_wait_flag_set(I2C1_SR1, I2C_SR1_SB);
 }
 
 void i2c_stop(void)
 {
-    *I2C3_CR1 |= I2C_CR1_STOP;
+    *I2C1_CR1 |= I2C_CR1_STOP;
 }
 
 static I2C_Status_t i2c_wait_flag_set(volatile uint32_t *reg, uint32_t flag)
@@ -82,20 +75,20 @@ static I2C_Status_t i2c_wait_flag_set(volatile uint32_t *reg, uint32_t flag)
 static void i2c_clear_addr_flag(void)
 {
     volatile uint32_t temp;
-    temp = *I2C3_SR1;
-    temp = *I2C3_SR2;
+    temp = *I2C1_SR1;
+    temp = *I2C1_SR2;
     (void)temp;
 }
 
 static I2C_Status_t i2c_send_address(uint8_t dev_addr, uint8_t read)
 {
     uint32_t timeout = I2C_TIMEOUT_COUNT;
-    *I2C3_DR = (uint32_t)((dev_addr << 1) | (read & 0x01));
-    while ((*I2C3_SR1 & I2C_SR1_ADDR) == 0)
+    *I2C1_DR = (uint32_t)((dev_addr << 1) | (read & 0x01));
+    while ((*I2C1_SR1 & I2C_SR1_ADDR) == 0)
     {
-        if ((*I2C3_SR1 & I2C_SR1_AF) != 0)
+        if ((*I2C1_SR1 & I2C_SR1_AF) != 0)
         {
-            *I2C3_SR1 &= ~I2C_SR1_AF;
+            *I2C1_SR1 &= ~I2C_SR1_AF;
             i2c_stop();
             return I2C_ERROR;
         }
@@ -111,60 +104,48 @@ static I2C_Status_t i2c_send_address(uint8_t dev_addr, uint8_t read)
 
 void i2c_init(void)
 {
-    clock_enable_AHB1(GPIOA_peripheral);
     clock_enable_AHB1(GPIOB_peripheral);
-    clock_enable_APB1(I2C3_peripheral);
+    clock_enable_APB1(I2C1_peripheral);
 
-    // PA8 SCL (AF4)
-    *GPIOA_MODER &= ~(0b11 << 16); 
-    *GPIOA_MODER |=  (0b10 << 16);
-    *GPIOA_OTYPER |= (1 << 8); // opendrain
-    *GPIOA_OSPEEDR &= ~(0b11 << 16);
-    *GPIOA_OSPEEDR |=  (0b10 << 16); // fast speed
-    *GPIOA_PUPDR &= ~(0b11 << 16);
-    *GPIOA_PUPDR |=  (0b01 << 16); // pull up
-    *GPIOA_AFRH &= ~(0xF << 0);
-    *GPIOA_AFRH |=  (4 << 0); // AF4
+    // PB6 = I2C1_SCL, PB7 = I2C1_SDA, AF4, open-drain, pull-up.
+    *GPIOB_MODER &= ~((0b11 << 12) | (0b11 << 14));
+    *GPIOB_MODER |=  ((0b10 << 12) | (0b10 << 14));
+    *GPIOB_OTYPER |= ((1 << 6) | (1 << 7));
+    *GPIOB_OSPEEDR &= ~((0b11 << 12) | (0b11 << 14));
+    *GPIOB_OSPEEDR |=  ((0b10 << 12) | (0b10 << 14));
+    *GPIOB_PUPDR &= ~((0b11 << 12) | (0b11 << 14));
+    *GPIOB_PUPDR |=  ((0b01 << 12) | (0b01 << 14));
+    *GPIOB_AFRL &= ~((0xF << 24) | (0xF << 28));
+    *GPIOB_AFRL |=  ((4 << 24) | (4 << 28));
 
-    // PB4 SDA (AF9)
-    *GPIOB_MODER &= ~(0b11 << 8); 
-    *GPIOB_MODER |=  (0b10 << 8);
-    *GPIOB_OTYPER |= (1 << 4); // opendrain
-    *GPIOB_OSPEEDR &= ~(0b11 << 8);
-    *GPIOB_OSPEEDR |=  (0b10 << 8); // fast speed
-    *GPIOB_PUPDR &= ~(0b11 << 8);
-    *GPIOB_PUPDR |=  (0b01 << 8); // pull up
-    *GPIOB_AFRL &= ~(0xF << 16);
-    *GPIOB_AFRL |=  (9 << 16); // AF9
+    *I2C1_CR1 |= I2C_CR1_SWRST;
+    *I2C1_CR1 &= ~I2C_CR1_SWRST; 
 
-    *I2C3_CR1 |= I2C_CR1_SWRST;
-    *I2C3_CR1 &= ~I2C_CR1_SWRST; 
-
-    *I2C3_CR1 &= ~I2C_CR1_PE;
-    *I2C3_CR2 = I2C_APB1_MHZ; // 50
-    *I2C3_CCR = I2C_STANDARD_CCR; // 250
-    *I2C3_TRISE = I2C_STANDARD_TRISE; // 51
-    *I2C3_OAR1 = (1 << 14); // Should always be kept at 1 by software
-    *I2C3_CR1 |= I2C_CR1_PE;
+    *I2C1_CR1 &= ~I2C_CR1_PE;
+    *I2C1_CR2 = I2C_APB1_MHZ; // 50
+    *I2C1_CCR = I2C_STANDARD_CCR; // 250
+    *I2C1_TRISE = I2C_STANDARD_TRISE; // 51
+    *I2C1_OAR1 = (1 << 14); // Should always be kept at 1 by software
+    *I2C1_CR1 |= I2C_CR1_PE;
 }
 
 I2C_Status_t i2c_send_byte(uint8_t data)
 {
-    if (i2c_wait_flag_set(I2C3_SR1, I2C_SR1_TXE) != I2C_OK)
+    if (i2c_wait_flag_set(I2C1_SR1, I2C_SR1_TXE) != I2C_OK)
     {
         return I2C_TIMEOUT;
     }
 
-    *I2C3_DR = data;
+    *I2C1_DR = data;
 
-    if (i2c_wait_flag_set(I2C3_SR1, I2C_SR1_TXE) != I2C_OK)
+    if (i2c_wait_flag_set(I2C1_SR1, I2C_SR1_TXE) != I2C_OK)
     {
         return I2C_TIMEOUT;
     }
 
-    if ((*I2C3_SR1 & I2C_SR1_AF) != 0)
+    if ((*I2C1_SR1 & I2C_SR1_AF) != 0)
     {
-        *I2C3_SR1 &= ~I2C_SR1_AF;
+        *I2C1_SR1 &= ~I2C_SR1_AF;
         return I2C_ERROR;
     }
 
@@ -180,19 +161,19 @@ I2C_Status_t i2c_read_byte(uint8_t *data, uint8_t ack)
 
     if (ack != 0)
     {
-        *I2C3_CR1 |= I2C_CR1_ACK;
+        *I2C1_CR1 |= I2C_CR1_ACK;
     }
     else
     {
-        *I2C3_CR1 &= ~I2C_CR1_ACK;
+        *I2C1_CR1 &= ~I2C_CR1_ACK;
     }
 
-    if (i2c_wait_flag_set(I2C3_SR1, I2C_SR1_RXNE) != I2C_OK)
+    if (i2c_wait_flag_set(I2C1_SR1, I2C_SR1_RXNE) != I2C_OK)
     {
         return I2C_TIMEOUT;
     }
 
-    *data = (uint8_t)*I2C3_DR;
+    *data = (uint8_t)*I2C1_DR;
     return I2C_OK;
 }
 
@@ -223,7 +204,7 @@ I2C_Status_t i2c_master_transmit(uint8_t dev_addr, const uint8_t *data, uint16_t
         }
     }
 
-    if (i2c_wait_flag_set(I2C3_SR1, I2C_SR1_BTF) != I2C_OK)
+    if (i2c_wait_flag_set(I2C1_SR1, I2C_SR1_BTF) != I2C_OK)
     {
         i2c_stop();
         return I2C_TIMEOUT;
@@ -247,18 +228,18 @@ I2C_Status_t i2c_master_receive(uint8_t dev_addr, uint8_t *data, uint16_t len)
 
     if (len == 1)
     {
-        *I2C3_CR1 &= ~I2C_CR1_POS;
-        *I2C3_CR1 &= ~I2C_CR1_ACK;
+        *I2C1_CR1 &= ~I2C_CR1_POS;
+        *I2C1_CR1 &= ~I2C_CR1_ACK;
     }
     else if (len == 2)
     {
-        *I2C3_CR1 |= I2C_CR1_ACK;
-        *I2C3_CR1 |= I2C_CR1_POS;
+        *I2C1_CR1 |= I2C_CR1_ACK;
+        *I2C1_CR1 |= I2C_CR1_POS;
     }
     else
     {
-        *I2C3_CR1 &= ~I2C_CR1_POS;
-        *I2C3_CR1 |= I2C_CR1_ACK;
+        *I2C1_CR1 &= ~I2C_CR1_POS;
+        *I2C1_CR1 |= I2C_CR1_ACK;
     }
 
     if (i2c_send_address(dev_addr, 1) != I2C_OK)
@@ -272,25 +253,25 @@ I2C_Status_t i2c_master_receive(uint8_t dev_addr, uint8_t *data, uint16_t len)
     {
         i2c_stop();
 
-        if (i2c_wait_flag_set(I2C3_SR1, I2C_SR1_RXNE) != I2C_OK)
+        if (i2c_wait_flag_set(I2C1_SR1, I2C_SR1_RXNE) != I2C_OK)
         {
             return I2C_TIMEOUT;
         }
 
-        data[0] = (uint8_t)*I2C3_DR;
+        data[0] = (uint8_t)*I2C1_DR;
     }
     else if (len == 2)
     {
-        *I2C3_CR1 &= ~I2C_CR1_ACK;
+        *I2C1_CR1 &= ~I2C_CR1_ACK;
 
-        if (i2c_wait_flag_set(I2C3_SR1, I2C_SR1_BTF) != I2C_OK)
+        if (i2c_wait_flag_set(I2C1_SR1, I2C_SR1_BTF) != I2C_OK)
         {
             return I2C_TIMEOUT;
         }
 
         i2c_stop();
-        data[0] = (uint8_t)*I2C3_DR;
-        data[1] = (uint8_t)*I2C3_DR;
+        data[0] = (uint8_t)*I2C1_DR;
+        data[1] = (uint8_t)*I2C1_DR;
     }
     else
     {
@@ -298,111 +279,35 @@ I2C_Status_t i2c_master_receive(uint8_t dev_addr, uint8_t *data, uint16_t len)
         {
             if (i == (uint16_t)(len - 3))
             {
-                if (i2c_wait_flag_set(I2C3_SR1, I2C_SR1_BTF) != I2C_OK)
+                if (i2c_wait_flag_set(I2C1_SR1, I2C_SR1_BTF) != I2C_OK)
                 {
                     return I2C_TIMEOUT;
                 }
 
-                *I2C3_CR1 &= ~I2C_CR1_ACK;
-                data[i++] = (uint8_t)*I2C3_DR;
+                *I2C1_CR1 &= ~I2C_CR1_ACK;
+                data[i++] = (uint8_t)*I2C1_DR;
 
-                if (i2c_wait_flag_set(I2C3_SR1, I2C_SR1_BTF) != I2C_OK)
+                if (i2c_wait_flag_set(I2C1_SR1, I2C_SR1_BTF) != I2C_OK)
                 {
                     return I2C_TIMEOUT;
                 }
 
                 i2c_stop();
-                data[i++] = (uint8_t)*I2C3_DR;
-                data[i] = (uint8_t)*I2C3_DR;
+                data[i++] = (uint8_t)*I2C1_DR;
+                data[i] = (uint8_t)*I2C1_DR;
                 break;
             }
 
-            if (i2c_wait_flag_set(I2C3_SR1, I2C_SR1_RXNE) != I2C_OK)
+            if (i2c_wait_flag_set(I2C1_SR1, I2C_SR1_RXNE) != I2C_OK)
             {
                 return I2C_TIMEOUT;
             }
 
-            data[i] = (uint8_t)*I2C3_DR;
+            data[i] = (uint8_t)*I2C1_DR;
         }
     }
 
-    *I2C3_CR1 &= ~I2C_CR1_POS;
-    *I2C3_CR1 |= I2C_CR1_ACK;
+    *I2C1_CR1 &= ~I2C_CR1_POS;
+    *I2C1_CR1 |= I2C_CR1_ACK;
     return I2C_OK;
-}
-
-I2C_Status_t i2c_mem_write(uint8_t dev_addr, uint8_t mem_addr, const uint8_t *data, uint16_t len)
-{
-    if ((data == 0) && (len > 0))
-    {
-        return I2C_ERROR;
-    }
-
-    if (i2c_start() != I2C_OK)
-    {
-        return I2C_TIMEOUT;
-    }
-
-    if (i2c_send_address(dev_addr, 0) != I2C_OK)
-    {
-        return I2C_ERROR;
-    }
-    i2c_clear_addr_flag();
-
-    if (i2c_send_byte(mem_addr) != I2C_OK)
-    {
-        i2c_stop();
-        return I2C_ERROR;
-    }
-
-    for (uint16_t i = 0; i < len; i++)
-    {
-        if (i2c_send_byte(data[i]) != I2C_OK)
-        {
-            i2c_stop();
-            return I2C_ERROR;
-        }
-    }
-
-    if (i2c_wait_flag_set(I2C3_SR1, I2C_SR1_BTF) != I2C_OK)
-    {
-        i2c_stop();
-        return I2C_TIMEOUT;
-    }
-
-    i2c_stop();
-    return I2C_OK;
-}
-
-I2C_Status_t i2c_mem_read(uint8_t dev_addr, uint8_t mem_addr, uint8_t *data, uint16_t len)
-{
-    if ((data == 0) || (len == 0))
-    {
-        return I2C_ERROR;
-    }
-
-    if (i2c_start() != I2C_OK)
-    {
-        return I2C_TIMEOUT;
-    }
-
-    if (i2c_send_address(dev_addr, 0) != I2C_OK)
-    {
-        return I2C_ERROR;
-    }
-    i2c_clear_addr_flag();
-
-    if (i2c_send_byte(mem_addr) != I2C_OK)
-    {
-        i2c_stop();
-        return I2C_ERROR;
-    }
-
-    if (i2c_wait_flag_set(I2C3_SR1, I2C_SR1_BTF) != I2C_OK)
-    {
-        i2c_stop();
-        return I2C_TIMEOUT;
-    }
-
-    return i2c_master_receive(dev_addr, data, len);
 }
