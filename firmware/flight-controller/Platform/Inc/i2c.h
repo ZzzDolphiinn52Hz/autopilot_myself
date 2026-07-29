474b15b82106d1425098d5df5dbaa0abeaaa3a6d
NTKkhanhnek <ntkkhanhnek06@gmail.com>
2026-07-29 22:17:23 +0700

    fix(i2c): Delete mem_read, mem_write function and use basic function such as transmit, receive to replace

--- firmware/flight-controller/Platform/Inc/i2c.h
+++ firmware/flight-controller/Platform/Inc/i2c.h
@@ -11,7 +11,9 @@ typedef enum
 } I2C_Status_t;
 
 void i2c_init(void);
-I2C_Status_t i2c_mem_write(uint8_t dev_addr, uint8_t mem_addr, const uint8_t *data, uint16_t len);
-I2C_Status_t i2c_mem_read(uint8_t dev_addr, uint8_t mem_addr, uint8_t *data, uint16_t len);
+I2C_Status_t i2c_send_byte(uint8_t data);
+I2C_Status_t i2c_read_byte(uint8_t *data, uint8_t ack);
+I2C_Status_t i2c_master_transmit(uint8_t dev_addr, const uint8_t *data, uint16_t len);
+I2C_Status_t i2c_master_receive(uint8_t dev_addr, uint8_t *data, uint16_t len);
 
 #endif /* INC_I2C_H_ */
